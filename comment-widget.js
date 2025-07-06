7018f9474cacc31f970fed865f497cd4dda24fbb
SquircleUnderscore <polosteor@gmail.com>
2025-07-06 12:01:28 +0200

    fix loading

--- comment-widget.js
+++ comment-widget.js
@@ -28,8 +28,8 @@ const s_replyId = '1190940416';
 const s_sheetId = '1zKJmcAodWjYHWAMKsAWRTTCbf4C0pJOOQWvSB1UNxpM';
 
 // The values below are necessary for accurate timestamps, I've filled it in with EST as an example
-const s_timezone = -5; // Your personal timezone (Example: UTC-5:00 is -5 here, UTC+10:30 would be 10.5)
-const s_daylightSavings = true; // If your personal timezone uses DST, set this to true
+const s_timezone = +2; // Your personal timezone (Example: UTC-5:00 is -5 here, UTC+10:30 would be 10.5)
+const s_daylightSavings = false; // If your personal timezone uses DST, set this to true
 // For the dates DST start and end where you live: [Month, Weekday, which number of that weekday, hour (24 hour time)]
 const s_dstStart = ['March', 'Sunday', 2, 2]; // Example shown is the second Sunday of March at 2:00 am
 const s_dstEnd = ['November', 'Sunday', 1, 2]; // Example shown is the first Sunday of November at 2:00 am
